f3bb12bf0de9db9bb08a3a405ee5c6f9f2012f46
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-10-22 16:42:17 +0200

    lab multiplyExact

--- src/_02_Typy/LightTravel.java
+++ src/_02_Typy/LightTravel.java
@@ -5,14 +5,17 @@ package _02_Typy;
 class LightTravel{
     public static void main(String args[]){
 	int lightSpeed = 299792; // km/s
-	long days = 100000000;        // badana liczba dni
+	long days = 1000000000;        // badana liczba dni
 	long seconds = days * 24 * 60 * 60;   // tyle to sekund
 	long distance = lightSpeed * seconds; // tyle przebedzie swiatlo
-
         // teraz odpowiedz
 	System.out.print("W ciagu " + days);
 	System.out.print(" dni swiatlo przebedzie okolo ");
 	System.out.println("\n" + distance + " kilometrow");
+
+	long distance2 = Math.multiplyExact(lightSpeed, seconds);
+	System.out.println("\n" + distance2 + " kilometrow");	
+
 	System.out.println(Long.MAX_VALUE);
 //	double a = 1;
 //	byte b = (byte)(a*a);
