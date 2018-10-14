31b17cdff406f9f8415301977339b8e2d28acbe9
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-10-14 11:15:46 +0200

    Punkt equals

--- src/Laboratorium/Geometria/Punkt.java
+++ src/Laboratorium/Geometria/Punkt.java
@@ -33,6 +33,25 @@ public class Punkt {
 		return "[ "+ x + ", " + y + " ]";
 	}
 	
+	@Override
+    public boolean equals(Object obj) {
+		if (this == obj)
+	            return true;
+	    if (obj == null)
+	            return false;
+	    if (this.getClass() != obj.getClass())
+	            return false;
+	        
+	    Punkt other = (Punkt) obj;
+	    if(this.getX() == other.getX())
+	    	if(this.getY() == other.getY())
+	    		return true;
+	    	else
+	    		return false;
+	    else
+	    	return false;
+	}
+	
 	public double odleglosc(Punkt p) {
 		double x2 = (this.getX() - p.getX())*(this.getX() - p.getX());
 		double y2 = (this.getY() - p.getY())*(this.getY() - p.getY());
