4a93de18712feb5c21703586423112e2451f4447
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-10-14 10:41:02 +0200

    Wyklad: Epsilon + Nan

---
+++ src/_02_Typy/Nan.java
@@ -0,0 +1,13 @@
+package _02_Typy;
+
+public class Nan {
+	public static void main(String[] args) {
+		
+		if((1.0 / 0) == Double.POSITIVE_INFINITY)
+			System.out.println("+ INF");
+		if((-1.0 / 0) == Double.NEGATIVE_INFINITY)
+			System.out.println("- INF");
+		if(Double.isNaN(0.0 / 0))
+			System.out.println("NaN");
+	}
+}
