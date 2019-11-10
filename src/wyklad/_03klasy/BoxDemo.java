b06f38a47f412edfe56055101d1a0649ebf95513
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2019-11-10 08:44:49 +0100

    refactor

--- src/wyklad/_03klasy/BoxDemo.java
+++ src/wyklad/_03klasy/BoxDemo.java
@@ -1,32 +1,24 @@
 package wyklad._03klasy;
 
-// Definicja prostej klasy (tylko dane, zadnych metod)
-
 class Box {
 	double width;
 	double height;
 	double depth;
 }
 
-// definicja klasy demonstrujacej uzycie klasy Box
-
 class BoxDemo {
 	public static void main(String args[]) {
-		// utworzenie instancji klasy Box
-		// od tego czasu zmienna myBox reprezentuje instancje klasy Box
 		Box myBox = new Box();
 		double vol;
 
-		// obliczamy objetosc myBox'a
 		vol = myBox.width * myBox.height * myBox.depth;
 
 		System.out.println("Objetosc = " + vol);
-		// wpisujemy dane do myBox
+
 		myBox.width = 10;
 		myBox.height = 20;
 		myBox.depth = 15;
 
-		// obliczamy objetosc myBox'a
 		vol = myBox.width * myBox.height * myBox.depth;
 
 		System.out.println("Objetosc = " + vol);
