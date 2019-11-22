c28cd200d32a8f1f84512033df79dfa6458209ce
kturk0 <46371512+kturk0@users.noreply.github.com>
2019-11-22 10:33:03 +0100

    Add files via upload

---
+++ KacperTurk_2019/fvat/fvat/sprzedajacy.java
@@ -0,0 +1,54 @@
+package fvat;
+
+public class sprzedajacy {
+    private String nazwa_firmy;
+    private String adres;
+    private String NIP;
+    private String nr_konta;
+
+    public sprzedajacy(){
+        nazwa_firmy = "TELPOL";
+        adres = "ul.Nosala 84, Warszawa";
+        NIP = "6574534670";
+        nr_konta = "77884337846";
+    }
+
+    public sprzedajacy(String nazwa_firmy, String adres, String NIP, String nr_konta){
+        this.nazwa_firmy = nazwa_firmy;
+        this.adres = adres;
+        this.NIP = NIP;
+        this.nr_konta = nr_konta;
+    }
+
+    public String getNazwa_firmy() {
+        return nazwa_firmy;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public String getNIP() {
+        return NIP;
+    }
+
+    public String getNr_konta() {
+        return nr_konta;
+    }
+
+    public void setNazwa_firmy(String nazwa_firmy) {
+        this.nazwa_firmy = nazwa_firmy;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
+    }
+
+    public void setNr_konta(String nr_konta) {
+        this.nr_konta = nr_konta;
+    }
+}
