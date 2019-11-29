2b613f04a6de11d638bf0be024a9625067bcba59
Kondrat Trunowicz <kondrattrunowicz@gmail.com>
2019-11-29 19:26:07 +0100

    Poprawiona ponownie wersja Banku i Koszyka

--- Studenci/KondratTrunowicz_2019/koszyk/CartTest.java
+++ Studenci/KondratTrunowicz_2019/koszyk/CartTest.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class CartTest {
     @Test
     public void sorting(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
-        Product product6 = new Product (6666,"Cola",10);
-        Product product5 = new Product (5555,"Dola",10);
-        Product product4 = new Product (4444,"Bola",10);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
+        Product product6 = new Product ("6666","Cola",10);
+        Product product5 = new Product ("5555","Dola",10);
+        Product product4 = new Product ("4444","Bola",10);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -30,9 +30,9 @@ public class CartTest {
     }
     @Test
     public void sortbyName(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -46,9 +46,9 @@ public class CartTest {
     }
     @Test
     public void sortbyPrice(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -62,12 +62,12 @@ public class CartTest {
     }
     @Test
     public void lowcost(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
-        Product product6 = new Product (6666,"Cola",10);
-        Product product5 = new Product (5555,"Dola",10);
-        Product product4 = new Product (4444,"Bola",10);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
+        Product product6 = new Product ("6666","Cola",10);
+        Product product5 = new Product ("5555","Dola",10);
+        Product product4 = new Product ("4444","Bola",10);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -79,12 +79,12 @@ public class CartTest {
     }
     @Test
     public void highcost(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
-        Product product6 = new Product (6666,"Cola",10);
-        Product product5 = new Product (5555,"Dola",10);
-        Product product4 = new Product (4444,"Bola",10);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
+        Product product6 = new Product ("6666","Cola",10);
+        Product product5 = new Product ("5555","Dola",10);
+        Product product4 = new Product ("4444","Bola",10);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -97,12 +97,12 @@ public class CartTest {
     @Test
     //For 3 products
     public void lowcostN(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
-        Product product6 = new Product (6666,"Cola",10);
-        Product product5 = new Product (5555,"Dola",10);
-        Product product4 = new Product (4444,"Bola",10);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
+        Product product6 = new Product ("6666","Cola",10);
+        Product product5 = new Product ("5555","Dola",10);
+        Product product4 = new Product ("4444","Bola",10);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -120,12 +120,12 @@ public class CartTest {
     @Test
     //For 3 products
     public void highostN(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
-        Product product3 = new Product (3333,"Komputer",200);
-        Product product6 = new Product (6666,"Cola",10);
-        Product product5 = new Product (5555,"Dola",10);
-        Product product4 = new Product (4444,"Bola",10);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
+        Product product3 = new Product ("3333","Komputer",200);
+        Product product6 = new Product ("6666","Cola",10);
+        Product product5 = new Product ("5555","Dola",10);
+        Product product4 = new Product ("4444","Bola",10);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -142,8 +142,8 @@ public class CartTest {
     }
     @Test
     public void addProduct(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -154,7 +154,7 @@ public class CartTest {
     }
     @Test
     public void getProductlist(){
-        Product product = new Product (1111,"Telewizor",100);
+        Product product = new Product ("1111","Telewizor",100);
         Cart cart = new Cart();
         cart.addProduct(product);
         ArrayList<Product> koszyk = new ArrayList<>();
@@ -163,8 +163,8 @@ public class CartTest {
     }
     @Test
     public void sumPrice(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",50);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",50);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.addProduct(product2);
@@ -173,8 +173,8 @@ public class CartTest {
     }
     @Test
     public void SumaRabat(){
-        Product product = new Product (1111,"Telewizor",210);
-        Product product2 = new Product (2222,"Laptop",100);
+        Product product = new Product ("1111","Telewizor",210);
+        Product product2 = new Product ("2222","Laptop",100);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product);
@@ -185,9 +185,9 @@ public class CartTest {
     }
     @Test
     public void conslusion300(){
-        Product product = new Product (1111,"Telewizor",200);
-        Product product2 = new Product (2222,"Laptop",100);
-        Product product3 = new Product (3333,"Cola",10);
+        Product product = new Product ("1111","Telewizor",200);
+        Product product2 = new Product ("2222","Laptop",100);
+        Product product3 = new Product ("3333","Cola",10);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product);
@@ -200,8 +200,8 @@ public class CartTest {
     }
     @Test
     public void conslusion200(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",100);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",100);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product);
@@ -213,9 +213,9 @@ public class CartTest {
     }
     @Test
     public void conslusion3items(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",100);
-        Product product3 = new Product (3333,"Cegła",50);
+        Product product = new Product ("1111","Telewizor",200);
+        Product product2 = new Product ("2222","Laptop",100);
+        Product product3 = new Product ("3333","Cegła",30);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product);
@@ -224,11 +224,11 @@ public class CartTest {
         cart.sumPrice();
         cart.conclusion();
         cart.SumaRabat();
-        Assert.assertEquals(cart.getSumaRabat(), 200,0.1);
+        Assert.assertEquals(cart.getSumaRabat(), 285,0.1);
     }
     @Test
     public void conclusionWithRabat(){
-        Product product = new Product (1111,"Telewizor",200);
+        Product product = new Product ("1111","Telewizor",200);
         Cart cart = new Cart();
         cart.addProduct(product);
         cart.sumPrice();
@@ -240,9 +240,9 @@ public class CartTest {
     }
     @Test
     public void conslusion3itemsWithRabat(){
-        Product product = new Product (1111,"Telewizor",100);
-        Product product2 = new Product (2222,"Laptop",100);
-        Product product3 = new Product (3333,"Cegła",50);
+        Product product = new Product ("1111","Telewizor",100);
+        Product product2 = new Product ("2222","Laptop",100);
+        Product product3 = new Product ("3333","Cegła",50);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product);
@@ -255,9 +255,9 @@ public class CartTest {
     }
     @Test
     public void conslusion300withRabat(){
-        Product product = new Product (1111,"Telewizor",300);
-        Product product2 = new Product (2222,"Laptop",100);
-        Product product3 = new Product (3333,"Cola",100);
+        Product product = new Product ("1111","Telewizor",200);
+        Product product2 = new Product ("2222","Laptop",100);
+        Product product3 = new Product ("3333","Cegła",50);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product);
@@ -266,18 +266,18 @@ public class CartTest {
         cart.sumPrice();
         cart.conclusionWithRabat("Telewizor");
         cart.SumaRabat();
-        Assert.assertEquals(cart.getSumaRabat(), 380,0.1);
+        Assert.assertEquals(cart.getSumaRabat(), 285,0.1);
     }
     @Test
     public void conslusion200withRabat(){
-        Product product2 = new Product (2222,"Laptop",100);
-        Product product3 = new Product (3333,"Cola",100);
+        Product product2 = new Product ("2222","Laptop",100);
+        Product product3 = new Product ("3333","Cegła",100);
         Cart cart = new Cart();
         cart.addProduct(product2);
         cart.addProduct(product3);
         cart.sorting();
         cart.sumPrice();
-        cart.conclusionWithRabat("Cola");
+        cart.conclusionWithRabat("Laptop");
         cart.SumaRabat();
         Assert.assertEquals(cart.getSumaRabat(), 170,0.1);
     }
