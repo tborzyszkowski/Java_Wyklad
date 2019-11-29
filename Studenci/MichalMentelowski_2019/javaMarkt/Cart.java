fe1ce42d3ee20b0f4c55367d5a608090417802ee
mmentelowski <m.mentelowski@gmail.com>
2019-11-29 11:07:06 +0100

    dalsza czesc javaMarkt

--- Studenci/MichalMentelowski_2019/javaMarkt/Cart.java
+++ Studenci/MichalMentelowski_2019/javaMarkt/Cart.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Cart {
     private ArrayList<Product> cart;
+    private String accesory;
 
     public Cart(){
         ArrayList<Product> cart = new ArrayList<Product>();
@@ -12,7 +13,38 @@ public class Cart {
         if(this.cart.size()==3){
             thirdGratis();
             }
+        else{
+            blackFriday();
         }
+        double sum = 0;
+        for (Product p : this.cart) {
+            sum += p.getPrice();
+        }
+        if(sum>200)
+            cart.accesory ="kubek";
+        if(sum>300)
+            for (Product p : this.cart) {
+                p.setDiscount(0.95);
+            }
+        }
+
+
+
+    public ArrayList<Product> getCart() {
+        return cart;
+    }
+
+    public void setCart(ArrayList<Product> cart) {
+        this.cart = cart;
+    }
+
+    public String getAccesory() {
+        return accesory;
+    }
+
+    public void setAccesory(String accesory) {
+        this.accesory = accesory;
+    }
 
     public double findCheapest() {
         double low = 99999999;
@@ -21,6 +53,19 @@ public class Cart {
                 low = p.getPrice();
         }
             return low;
+    }
+    public ArrayList<Product> findCheapestN(int n) {
+        double low = 99999999;
+        ArrayList<Product> tab = new ArrayList<>();
+        for (int i = n;i>0;i--) {
+            for (Product p : this.cart) {
+                if (p.getPrice() < low) {
+                    low = p.getPrice();
+                    tab.add()
+                }
+            }
+        }
+        return tab;
     }
 
     public double findMostExpensive(){
@@ -31,6 +76,7 @@ public class Cart {
             }
             return high;
     }
+   //DISCOUNTS
     public void thirdGratis(){
         double low = findCheapest();
         for(Product p:this.cart){
@@ -38,5 +84,13 @@ public class Cart {
                 p.setPrice(0);
         }
     }
+    public void blackFriday(){
+        double low = findCheapest();
+        for(Product p:this.cart){
+            if(p.getPrice()==low)
+                p.setDiscount(0.70);
+        }
+    }
+
 
 }
--- Studenci/MichalMentelowski_2019/javaMarkt/Product.java
+++ Studenci/MichalMentelowski_2019/javaMarkt/Product.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 
 public class Product {
     private String code;
-    private  String name;
-    private  double price;
+    private String name;
+    private double price;
     private double discountPrice;
     private double discount;
-    private String accesory;
 
     public Product(String code,String name,double price){
         this.code = code;
@@ -48,8 +47,15 @@ public class Product {
     public void setDiscountPrice(double discountPrice) {
         this.discountPrice = discountPrice;
     }
+    public double getDiscount() {
+        return discountPrice;
+    }
 
-    public void discounterP(Product product){
+    public void setDiscount(double discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    /*public void discounterP(Product product){
         if(price>300){
             this.discount = 0.95;
         }
@@ -60,5 +66,5 @@ public class Product {
             this.accesory = "kubek";
         }
 
-    }
+    }*/
 }
