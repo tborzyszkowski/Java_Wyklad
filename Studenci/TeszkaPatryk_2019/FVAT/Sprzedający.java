679bf789f486a192bcbfba4180ca58515af1dd7d
pteszka <46346427+pteszka@users.noreply.github.com>
2019-11-29 23:14:19 +0100

    Poprawa Sprzedający.java

--- Studenci/TeszkaPatryk_2019/FVAT/Sprzedający.java
+++ Studenci/TeszkaPatryk_2019/FVAT/Sprzedający.java
@@ -17,10 +17,7 @@ public class Sprzedający {
     public Sprzedający(Sprzedający sprzedający) {this(sprzedający.nazwa_firmy, sprzedający.nip, sprzedający.adres, sprzedający.nr_konta);}
 
     public Sprzedający() {
-        this.nazwa_firmy =  "Firemka";
-        this.nip = "8951253";
-        this.adres = "Gdańsk, Budowlancow 555/0";
-        this.nr_konta = "2249000054526807771493031";
+        this("Firemka", "8951253", "Gdańsk, Budowlancow 555/0", "2249000054526807771493031");
     }
 
     public String getNr_konta() {
